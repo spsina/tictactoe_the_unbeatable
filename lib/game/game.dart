cf101542df387613d14cd6b3030a14c1552b32ae
Ali Parvizi <snparvizi75@gmail.com>
2020-04-13 14:42:39 +0430

    assets for battle select page and battle page ai thinking asset | basic tictactoe game class

---
+++ lib/game/game.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class Tictactoe extends Game{
+  Size screenSize;
+  Size tileSize;
+
+
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:tictactoe/game/game.dart';
 
-void main() {}
+void main() {
+  Util flameUtil = Util();
+  flameUtil.fullScreen();
+  flameUtil.setOrientation(DeviceOrientation.portraitUp);
+  Tictactoe game = Tictactoe();
+
+  runApp(game.widget);
+}
