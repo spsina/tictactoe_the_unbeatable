e57ee7a6d9722b5402a5e1e5833f7762844163c9
Ali Parvizi <snparvizi75@gmail.com>
2020-04-14 16:26:55 +0430

    your turn, ai turn animation

--- lib/pages/generic/turn.dart
+++ lib/pages/generic/turn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tictactoe/game/player.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 class Trun extends StatelessWidget{
   final Player player;
@@ -13,29 +14,29 @@ class Trun extends StatelessWidget{
     double tileSize = MediaQuery. of(context).size.width / 9;
 
     return Container(
-      margin: EdgeInsets.only(top: tileSize),
+      margin: EdgeInsets.only(top: tileSize / 2),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Column(
             children: <Widget>[
+              Container(
+                width: tileSize * 4,
+                height: tileSize * 4,
+                margin: EdgeInsets.only(top:tileSize / 5),
+                child: FlareActor(
+                  isX ? "assets/animations/battle/your_turn.flr"
+                        :"assets/animations/battle/thinking.flr", 
+                  alignment:Alignment.center, 
+                  sizeFromArtboard: true, 
+                  animation:"Alarm"
+                )
+              ),
               Text(isX ? "YOUR TURN" : "AI'S TURN", style: TextStyle(
                   color: Color(0xffF4F4F4),
-                  fontSize: tileSize
+                  fontSize: tileSize / 4
                 ),
               ),
-
-              Container(
-                margin: EdgeInsets.only(top:tileSize / 5),
-                child: Image(
-                  image: AssetImage( isX ?
-                                    "assets/images/battleSelect/your_turn.png"
-                                    :"assets/images/battleSelect/ai_turn.png"
-                                    ),
-                    width: tileSize * 3,
-                    height: tileSize * 3,
-                  ),
-              )
             ],
           )
         ],
