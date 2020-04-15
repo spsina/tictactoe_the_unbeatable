cbd2e92321dd047f45804ffb2fa3c94c15835b58
Ali Parvizi <snparvizi75@gmail.com>
2020-04-15 16:17:25 +0430

    GameBoard, modified to work with Board internally

--- lib/game/player.dart
+++ lib/game/player.dart
@@ -5,8 +5,14 @@ enum PlayerType {
 
 class Player {
   PlayerType type;
+  int key;
 
-  Player(this.type);
+  Player(this.type){
+    if (type == PlayerType.X)
+      key = 0;
+    else
+      key = 1;
+  }
 
   String getRepresentation() {
     if (type == PlayerType.X)
--- lib/pages/battle/battle.dart
+++ lib/pages/battle/battle.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tictactoe/game/player.dart';
+import 'package:tictactoe/game/board.dart';
 import 'package:tictactoe/main.dart';
 import 'package:tictactoe/pages/battleSelect/battleSelect.dart';
 import 'package:tictactoe/pages/generic/turn.dart';
-import 'package:tuple/tuple.dart';
-
 
 
 class GameBoard extends StatefulWidget {
@@ -22,45 +20,21 @@ class GameBoard extends StatefulWidget {
 
 class Game extends State<GameBoard> {
   
-  List<List<String>> _board;
-  Player turn = Player(PlayerType.X);
-  Turn turnWidget = Turn(Player(PlayerType.X), 0);
-  Tuple2<int,int> lastMove = Tuple2<int, int> (-1, -1);
+  Board board;
+  Turn turnWidget;
 
   @override
   void initState() {
-      _board = List<List<String>>.generate(widget.size, (i) { 
-        return List<String>.generate(widget.size, (j) => ""); 
-      });
-      super.initState();
+    board = Board(widget.size);
+    turnWidget = Turn(board.player, board.player.key);
+    super.initState();
   }
 
-  void moveTo(Tuple2<int, int> dest){
+  void moveTo(i, j) {
     setState(() {
-      int i = dest.item1; int j = dest.item2;
-      if (_board[i][j] == ""){
-        _board[i][j] = turn.getRepresentation();
-        lastMove = dest;
-      }
-        turn = count("X") == count("O") ? 
-          Player(PlayerType.X): Player(PlayerType.O);
-        if (turn.type == PlayerType.X)
-          turnWidget = Turn(turn, 0);
-        else
-          turnWidget = Turn(turn, 1);
+      board.moveTo(i, j);
+      turnWidget = Turn(board.player, board.player.key);
     });
-  }
-
-  int count(String target){
-    int c = 0;
-    _board.forEach((row) {
-      row.forEach((cell) {
-        if (cell == target)
-          c++;
-      });
-    });
-
-    return c;
   }
 
   @override
@@ -82,34 +56,32 @@ class Game extends State<GameBoard> {
             child: Column (
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               // a list of row, each containig a list of cols
-              children: List<Widget>.generate(widget.size, (i) {
+              children: List<Widget>.generate(board.size, (i) {
                 // generate a row
                 return Container(
                   margin: EdgeInsets.only(top: tileSize/5, right: 10, left:10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: List<Widget>.generate(widget.size, (j) {
+                    children: List<Widget>.generate(board.size, (j) {
                       // each cell
                       return Material(
-                        color: (i == lastMove.item1 && j == lastMove.item2) ? Color(0xffffac41) : Color(0xffd3d6db),
+                        color: (i == board.lastMove.item1 && j == board.lastMove.item2) ? Color(0xffffac41) : Color(0xffd3d6db),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)
                         ),
                         child: InkWell(
                           splashColor: Color(0xfff4f4f4),
                           onTap: (){ 
-                            setState(() {
-                              moveTo(Tuple2<int,int>(i,j));
-                            });
+                            moveTo(i, j);
                           },
                           child: Container(
                             
-                            width: (9 - (widget.size+1)*0.2)/widget.size * tileSize,
+                            width: (9 - (board.size+1)*0.2)/widget.size * tileSize,
                             height: (9 - (widget.size+1)*0.3)/widget.size * tileSize,
                             child: Center(
-                              child: Text(_board[i][j], textAlign: TextAlign.center, style: TextStyle(
+                              child: Text(board.board[i][j], textAlign: TextAlign.center, style: TextStyle(
                                   fontSize: ((9 - (widget.size+1)*0.3)/widget.size * tileSize ) * 0.5,
-                                  color: _board[i][j] == "X" ? Color(0xff11999e):Color(0xff3c3c3c)
+                                  color: board.board[i][j] == "X" ? Color(0xff11999e):Color(0xff3c3c3c)
                                 ),
                               ),
                             )
