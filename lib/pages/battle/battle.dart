8e160cd0aa1e8023118299684feab7dc9f5a6702
Ali Parvizi <snparvizi75@gmail.com>
2020-04-24 00:42:02 +0430

    battle page navigation floating action bar

--- lib/pages/battle/battle.dart
+++ lib/pages/battle/battle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:tictactoe/game/ai.dart';
 import 'package:tictactoe/game/board.dart';
 import 'package:tictactoe/main.dart';
@@ -181,51 +182,54 @@ class Game extends State<GameBoard> {
                   }),
                 )
             ),
-            Expanded(
-              child: Align(
-                child: Container(
-                    height: tileSize,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        FlatButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Entry(BattleSelectPage())),
-                              );
-                            },
-                            child: Container(
-                                width: tileSize * 3,
-                                height: tileSize,
-                                child: Center(
-                                    child: Icon(Icons.home, size: 50, color: Color(0xff29a19c),)
-                                )
-                            )
-                        ),
-                        FlatButton(
-                            onPressed: () {
-                              setState(() {
-                                initialize();
-                              });
-                            },
-                            child: Container(
-                                width: tileSize * 3,
-                                height: tileSize,
-                                child: Center(
-                                    child: Icon(Icons.replay, size: 50, color: Color(0xff29a19c),)
-                                )
-                            )
-                        ),
-                      ],
-                    )
-                ),
-                alignment: Alignment.bottomCenter,
-
-              ),
-            )
           ],
         ),
+      ),
+      floatingActionButton: SpeedDial(
+        // both default to 16
+        marginRight: 18,
+        marginBottom: 20,
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        // this is ignored if animatedIcon is non null
+        // child: Icon(Icons.add),
+        visible: true,
+        // If true user is forced to close dial manually
+        // by tapping main button and overlay is not rendered.
+        closeManually: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.home),
+              backgroundColor: Colors.red,
+              label: 'HOME',
+              labelStyle: TextStyle(fontSize: 14.0),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Entry(BattleSelectPage())),
+                );
+              }
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.replay),
+            backgroundColor: Colors.blue,
+            label: 'RESTART THE GAME',
+            labelStyle: TextStyle(fontSize: 14.0),
+            onTap: () => initialize()
+
+          ),
+        ],
       ),
     );
   }
