5ab643077c64f548d29d064a367c35febd8cd551
Ali Parvizi <snparvizi75@gmail.com>
2020-04-25 00:34:22 +0430

    websocket connection, get board id, loading and error management

--- lib/pages/battleSelect/customBoardPage.dart
+++ lib/pages/battleSelect/customBoardPage.dart
@@ -1,20 +1,84 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:tictactoe/pages/battle/battle.dart';
-import 'package:tictactoe/pages/battleSelect/components/topTitle.dart';
 import 'package:tictactoe/pages/generic/helper.dart';
+import 'package:web_socket_channel/io.dart';
 
 import 'battleSelect.dart';
 import 'components/customBoard.dart';
 
-class CustomBoardPage extends StatelessWidget{
+class CustomBoardPage extends StatefulWidget{
+  @override
+  _CustomBoardPageState createState() => _CustomBoardPageState();
+}
+
+class _CustomBoardPageState extends State<CustomBoardPage> {
+  IOWebSocketChannel channel;
+  WebSocket socket;
+  Widget buttonChild;
+  bool loading = false;
 
   final _customBoard = CustomBoard();
+
+  void socketListener(dynamic message) {
+    var dictData = jsonDecode(message.toString());
+    print("[Server] " + dictData.toString());
+  }
+
+  Future<IOWebSocketChannel> createConnection(url) async {
+    socket = await WebSocket
+        .connect(url)
+        .timeout(Duration(seconds: 15));
+    channel = IOWebSocketChannel(socket);
+    channel.stream.listen(this.socketListener);
+    return channel;
+  }
+
+  void requestGameId(request) async{
+    setState(() {
+      loading = true;
+    });
+    try {
+      var jsonData = jsonEncode(request);
+      await createConnection("ws://10.0.2.2:9091");
+      channel.sink.add(jsonData);
+    } catch (err) {
+      setState(() {
+        loading = false;
+        Fluttertoast.showToast(
+            msg: "Could not connect to the server",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    socket.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     double tileSize = MediaQuery. of(context).size.width / 9;
+
+    if (!loading){
+      buttonChild = Icon(Icons.play_arrow, color: Colors.white,);
+    } else {
+      buttonChild = CircularProgressIndicator(backgroundColor: Colors.white);
+    }
 
     return Scaffold(
       backgroundColor: Color(0xff1B2429),
@@ -56,6 +120,8 @@ class CustomBoardPage extends StatelessWidget{
             ),
             InkWell(
               onTap: () {
+                if (loading)
+                  return;
                 if (_customBoard.currentState.gameMode != GameMode.ONLINE) {
                   GameBoard game = GameBoard(
                     size: _customBoard.currentState.boardSize.toInt(),
@@ -65,6 +131,15 @@ class CustomBoardPage extends StatelessWidget{
                     winBy: _customBoard.currentState.winBy.toInt(),
                   );
                   navigate(context, game);
+                } else {
+                  // create an online game
+                  var request = {
+                    "type": "POST",
+                    "size": _customBoard.currentState.boardSize.toInt(),
+                    "winBy": _customBoard.currentState.winBy.toInt(),
+                    "starter": _customBoard.currentState.starter
+                  };
+                  requestGameId(request);
                 }
               },
               child: Container(
@@ -72,7 +147,7 @@ class CustomBoardPage extends StatelessWidget{
                 padding: EdgeInsets.all(10),
                 color: Color(0xffff1e56),
                 child: Center(
-                  child: Icon(Icons.play_arrow, color: Colors.white,),
+                  child: buttonChild,
                 ),
               ),
             )
@@ -108,5 +183,4 @@ class CustomBoardPage extends StatelessWidget{
       ),
     );
   }
-
 }
