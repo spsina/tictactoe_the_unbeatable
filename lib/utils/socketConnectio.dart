37ac79b3408d1ab368dfb48eb49e89950456f479
Ali Parvizi <snparvizi75@gmail.com>
2020-04-27 23:02:29 +0430

    untested socket connection utility

---
+++ lib/utils/socketConnectio.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:tictactoe/pages/generic/helper.dart';
+import 'package:web_socket_channel/io.dart';
+
+class WebSocketConnection {
+  final String url;
+  final int timeout;                     // socket connection timeout in seconds, 15 is default
+  WebSocket _socket;                     // the underlying socket connection
+  IOWebSocketChannel _channel;           // Channel wrapper
+  Set<Function> _subscribers;            // subscribers are functions that will be called on data event
+  bool isOn = false;                     // indicates that a live connection is established with the server
+
+  WebSocketConnection({this.url, this.timeout=15}){
+    _subscribers = Set();
+  }
+
+  void establishConnection() async {
+    // establish a connection with the server
+    try {
+      _socket = await WebSocket
+          .connect(url)
+          .timeout(Duration(seconds: timeout));
+      _channel = IOWebSocketChannel(_socket);
+      _channel.stream.listen(masterListener, onDone: connectionDropped);
+      isOn = true;
+    } catch(err) {
+      toastError("Could not connect to the server");
+      isOn = false;
+    }
+  }
+
+  void connectionDropped() {
+
+    // notify subscribers
+    // each subscriber will receive
+    // a message indicating that the connection is dropped
+
+    var droppedMsg = {
+      'status': -1,
+      'details': "Connection dropped"
+    };
+
+    masterListener(jsonEncode(droppedMsg));
+
+    // web socket connection has dropped
+    isOn = false;
+  }
+
+  void subscribe(Function func) {
+    // add function to subscribers set
+    _subscribers.add(func);
+  }
+
+  void unsubscribe(Function func) {
+    // remove function from subscribers list
+    _subscribers.remove(func);
+  }
+
+  void send(dynamic dictMsg) {
+    // send json encoded dictMsg to the server
+    if (isOn)
+      _channel.sink.add(jsonEncode(dictMsg));
+    else
+      toastError("No active connection to the server");
+  }
+
+  void masterListener(dynamic msg) {
+    // master socket listener
+    // all the subscribed functions
+    // will be called with a json decoded msg
+
+    var jsonMsg = jsonDecode(msg);
+
+    _subscribers.forEach((subscriber) {
+      subscriber(jsonMsg);
+    });
+  }
+
+  void dispose(){
+    _socket.close();
+  }
+}
