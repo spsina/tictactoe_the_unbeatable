18bc15d405d61f8eefc66bcda11fbe4237993884
FaezeShakuri <f.shakori.com@gmail.com>
2020-04-30 12:46:53 +0430

    custon board uptaded

--- lib/pages/battleSelect/components/customBoard.dart
+++ lib/pages/battleSelect/components/customBoard.dart
@@ -53,7 +53,7 @@ class _CustomBoardState extends State<CustomBoard> {
               Container(
                 child: DropdownButton<String>(
                   underline: SizedBox(),
-                  dropdownColor: Color(0xff323232),
+                  //dropdownColor: Color(0xff323232),
                   value: aiPlayer,
                   elevation: 20,
                   onChanged: (String value) {
@@ -137,7 +137,7 @@ class _CustomBoardState extends State<CustomBoard> {
                   child: Container(
                     width: 150,
                     child: DropdownButton<GameMode>(
-                      dropdownColor: Color(0xff323232),
+                      //dropdownColor: Color(0xff323232),
                       underline: SizedBox(),
                       isExpanded: true,
                       isDense: false,
@@ -179,7 +179,7 @@ class _CustomBoardState extends State<CustomBoard> {
                 Container(
                   child: DropdownButton<String>(
                     underline: SizedBox(),
-                    dropdownColor: Color(0xff323232),
+                   // dropdownColor: Color(0xff323232),
                     value: starter,
                     elevation: 20,
                     onChanged: (String value) {
